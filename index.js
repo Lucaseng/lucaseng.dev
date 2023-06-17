b36cfc4f9e63b2fe78db16be4526a8cd34bcb5f2
lucaseng <lucasengnz@outlook.com>
2023-06-17 15:39:32 +1200

    Home page created and partly populated

---
+++ index.js
@@ -0,0 +1,13 @@
+let i = 0;
+let txt = "I’m a recent Computer Science and Statistics Graduate.";
+let speed = 40;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = typeWriter();
