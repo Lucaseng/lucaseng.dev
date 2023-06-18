afdbe0baa59d9d2ae2682eccb42cd42995b0cce0
lucaseng <lucasengnz@outlook.com>
2023-06-18 14:18:02 +1200

    Added typewriterJS v2 for effective animations

--- index.js
+++ index.js
@@ -2,17 +2,63 @@ new fullpage("#fullpage", {
   // Navigation
   loopTop: true,
   loopBottom: true,
+  anchors: ["p1", "p2", "p3"],
+  afterLoad: function (origin, destination, direction, trigger) {
+    if (destination.anchor === "p3") {
+      techSkillTypeWriter();
+    }
+  },
 });
 
-let i = 0;
-let txt = "> I’m a recent Computer Science and Statistics Graduate.";
-let speed = 40;
+let myArr = [
+  `mySkills.forEach((skill) => {console.log(skill)})`,
+  "JavaScript (ES6+, Node.js)",
+  "Java",
+  "Python",
+  "C#",
+  "AWS",
+  "Git",
+  "R",
+  "SQL",
+  "HTML",
+  "CSS",
+  "Proof Methods",
+  "Probability & Probability Distributions",
+  "Markov Chains",
+  "Linear Algebra",
+  "Basic Data Analysis (Regression)",
+  ">>",
+];
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+  let typewriter = new Typewriter(document.getElementById("demo"), {
+    loop: false,
+    delay: 30,
+  });
+
+  typewriter
+    .typeString("> I’m a recent Computer Science and Statistics Graduate.")
+    .start();
+}
+
+function techSkillTypeWriter() {
+  var app = document.getElementById("mySkills");
+  if (app.innerText.length === 234) return;
+
+  var typewriter = new Typewriter(app, {
+    loop: false,
+    delay: 30,
+  });
+
+  typewriter
+    .typeString(`mySkills.forEach((skill) => {csl.log(skill)})`)
+    .pauseFor(600)
+    .deleteChars(15)
+    .typeString(`onsole.log(skill)}<br>`)
+    .changeDelay(20);
+
+  for (let i = 1; i < myArr.length; i++) {
+    typewriter.typeString("<br>" + myArr[i]).start();
   }
 }
 
